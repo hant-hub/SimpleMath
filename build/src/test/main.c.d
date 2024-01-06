bf0de57417695c09e269cd8448f8314e90a53a0e
Jeeli <elihantman@gmail.com>
2024-01-06 15:21:06 -0800

    Clamp function

--- build/src/test/main.c.d
+++ build/src/test/main.c.d
@@ -1,10 +1,12 @@
 build/./src/test/main.c.o: src/test/main.c src/test/logger.h \
  src/test/misc_test.h src/test/../include/misc/sqrt.h src/test/vec_test.h \
  src/test/../include/linAlg/types.h src/test/../include/linAlg/vec2.h \
- src/test/../include/linAlg/types.h src/test/../include/linAlg/ivec2.h \
- src/test/../include/linAlg/vec3.h src/test/../include/linAlg/ivec3.h \
- src/test/../include/linAlg/vec4.h src/test/../include/linAlg/ivec4.h \
- src/test/mat_test.h src/test/../include/linAlg/mat2x2.h \
+ src/test/../include/linAlg/types.h \
+ src/test/../include/linAlg/../misc/sqrt.h \
+ src/test/../include/linAlg/ivec2.h src/test/../include/linAlg/vec3.h \
+ src/test/../include/linAlg/ivec3.h src/test/../include/linAlg/vec4.h \
+ src/test/../include/linAlg/ivec4.h src/test/mat_test.h \
+ src/test/../include/linAlg/mat2x2.h \
  src/test/../include/linAlg/../error.h src/test/../include/linAlg/vec2.h \
  src/test/../include/linAlg/mat3x3.h src/test/../include/linAlg/vec3.h \
  src/test/../include/linAlg/mat4x4.h src/test/../include/linAlg/vec4.h \
@@ -30,6 +32,8 @@ src/test/../include/linAlg/types.h:
 src/test/../include/linAlg/vec2.h:
 
 src/test/../include/linAlg/types.h:
+
+src/test/../include/linAlg/../misc/sqrt.h:
 
 src/test/../include/linAlg/ivec2.h:
 
